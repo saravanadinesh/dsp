4468a962e1ed95d80c73302da53fdb143e41196a
Dinesh Thogulua <saravanadinesh@gmail.com>
2021-09-06 17:59:03 +0530

    Play functionality added. Comments added to code. Style (CSS) refined

--- wavinspector/main.py
+++ wavinspector/main.py
@@ -5,7 +5,6 @@ import numpy as np
 from scipy.signal import get_window
 from scipy.fft import rfft, fftfreq
 import sys
-import errno
 import os
 
 import dash
@@ -13,10 +12,9 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 import plotly.graph_objects as go
-import plotly.express as px
-import pandas as pd
 
 wf = None
+selected_section = np.array([])
 temp_tdfig = {'data':[],
               'layout': go.Layout(
                             xaxis_title = 'time (sec)',
@@ -46,10 +44,10 @@ temp_fdfig = {'data':[],
                             yaxis={'range':[-100, 0]}
                         )
             }
-old_figure = temp_tdfig, temp_fdfig
+old_figure = temp_tdfig, temp_fdfig # We are doing this so that even before we have some chart to show,
+                                    # we wan't to display empty charts with proper layout
 
-# We are good to get the html aoo setup
-# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+# App layout
 app = dash.Dash(__name__)
 app.config.suppress_callback_exceptions = True
 
@@ -58,7 +56,7 @@ app.layout = html.Div(
         html.Div(
             className='row',
             children=[
-                html.Div(className='three columns div-user-controls',
+                html.Div(className='three columns div-user-controls',   # This is the left block of the app
                          children=[
                             html.Div(
                                 style={'height':'20%'},
@@ -100,11 +98,11 @@ app.layout = html.Div(
                                         value='boxcar'
                                    )
                                 ]
-                            )
-                            
+                            ),
+                            html.Button('Play selected', id='play', n_clicks=0, style={'color': '#FF4F00', 'margin-top':'30px'})
                          ],
                 ),
-                html.Div(className='nine columns div-for-charts bg-grey',
+                html.Div(className='nine columns div-for-charts bg-grey',   # This is the right block of the app
                          children=[
                             dcc.Graph(id='td-graph', config={'displayModeBar': False}, style={'height':'45%'}, figure=temp_tdfig),
                             html.Div(
@@ -132,19 +130,27 @@ app.layout = html.Div(
     ]
 )
 
+
+#------------------------------------------------------------------------------------
+# getminmax
+#   Find the audio signal's range so that we can have a fixed y-axis scaling as we 
+# scroll through the audio data using the main slider
+# -----------------------------------------------------------------------------------
 def getminmax():
     global wf
     
     ymin = 0
     ymax = 0
-    for fs in range(wf.getnframes()//wf.getframerate()+1):
+    for fs in range(wf.getnframes()//wf.getframerate()+1): # Process 1 sec of data at a time
+                                                           # to prevent overuse of RAM
         wf.setpos(wf.getframerate()*fs)
         rawdata = wf.readframes(wf.getframerate()) # Read 1 sec worth of samples
         dt = {1:np.int8, 2:np.int16, 4:np.int32}.get(wf.getsampwidth())
         if dt == None: # We don't support other sample widths, Ex: 24-bit
             return 0,0
-        temp = np.frombuffer(rawdata, dtype=dt)
-        npdata = temp.reshape((wf.getnchannels(),-1), order='F') # If the wav fiel is stereo, then we will get a 2-row numpy array
+        temp = np.frombuffer(rawdata, dtype=dt) # Converting bytes object to np array
+        npdata = temp.reshape((wf.getnchannels(),-1), order='F') # If the wav fiel is stereo, 
+                                                                 # then we will get a 2-row numpy array
         if wf.getnchannels() > 2:
             return 0,0
         else:
@@ -154,8 +160,14 @@ def getminmax():
     return ymin, ymax
                 
         
+#------------------------------------------------------------------------------------
+# dsp_task
+#   This is the core task for this app. It multiplies a window function with the data
+# selected using the rangle slider (orange section). Then it takes FFT and calculates
+# the magnitude specturm.  
+# -----------------------------------------------------------------------------------
 def dsp_task(channel_value, fft_window, frame_start, window_of_interest, ymin, ymax):
-    global wf, old_figure
+    global wf, selected_section, old_figure
     
     if wf == None:
         return old_figure 
@@ -165,7 +177,9 @@ def dsp_task(channel_value, fft_window, frame_start, window_of_interest, ymin, y
     T_S = 1/F_S
     num_frames = wf.getnframes()
 
-    wf.setpos(round(num_frames*frame_start))
+    wf.setpos(round(num_frames*frame_start)) # Use the main slider location as the -
+                                             # starting point of data we fetch into
+                                             # the RAM
     rawdata = wf.readframes(F_S) # Read 1 sec worth of samples
     dt = {1:np.int8, 2:np.int16, 4:np.int32}.get(wf.getsampwidth())
     if dt == None: # We don't support other sample widths, Ex: 24-bit
@@ -182,7 +196,14 @@ def dsp_task(channel_value, fft_window, frame_start, window_of_interest, ymin, y
     
     subdata_start = round(window_of_interest[0]*F_S)
     subdata_end = round(window_of_interest[1]*F_S)
-    subdata = npdata[channel, subdata_start:subdata_end+1]
+    subdata = npdata[channel, subdata_start:subdata_end+1] # One can use some extra -
+                                                           # maths and directly read
+                                                           # the subdata of interest
+                                                           # into the RAM. I have avoided
+                                                           # this in the interest of 
+                                                           # making the code easily 
+                                                           # understandable
+    selected_section = subdata
 
     xmin = frame_start*num_frames/F_S  
     xmax = xmin + 1 
@@ -204,10 +225,10 @@ def dsp_task(channel_value, fft_window, frame_start, window_of_interest, ymin, y
                         yaxis={'range': [ymin, ymax]})
             }
 
-    N = subdata.shape[0]
-    fddata = rfft(subdata*get_window(fft_window, N))
+    N = subdata.shape[0] # We use the exactly the same N-point FFT as the t-domain number of samples
+    fddata = rfft(subdata*get_window(fft_window, N)) # Returns just one side of the FFT
     magresp = 20*np.log10(2*(1/N)*np.abs(fddata)/(2**(wf.getsampwidth()*8-1))) # dBFS
-    freqs = np.abs(fftfreq(N, T_S)[:N//2+1])
+    freqs = np.abs(fftfreq(N, T_S)[:N//2+1]) 
     fdfig = {'data': [go.Scatter(x=freqs, y=magresp, mode='lines')],
              'layout': go.Layout(
                         title={'text': 'Frequency response', 'font': {'color': 'white'}, 'x': 0.5},
@@ -223,6 +244,7 @@ def dsp_task(channel_value, fft_window, frame_start, window_of_interest, ymin, y
             }
     return tdfig, fdfig
 
+# Main callback function. Most inputs, all outtputs have been lumped up into this function
 @app.callback(Output('td-graph', 'figure'),
                 Output('fd-graph', 'figure'),
                 Output('filename-output', 'children'), 
@@ -241,15 +263,14 @@ def update_output(input_filename, submit_times, channel_value, fft_window, frame
     cross_mark = '\u274C', {'font-family': 'wingdings', 'color':'Crimson', 'font-size':'70%', 'padding-left':'30px'}
     
 
-    ctx = dash.callback_context
+    ctx = dash.callback_context # Since there are too many inputs, find which one actually triggered the callback
     triggered_input_id = ctx.triggered[0]['prop_id'].split('.')[0]
 
     
-    if triggered_input_id == 'filename':
+    if triggered_input_id == 'filename': # If filename is valid, call dsp task and plot the charts for the first time
         if submit_times == (update_output.submit_times+1):
             update_output.submit_times = update_output.submit_times + 1
             if os.path.exists(input_filename):
-                # Read wav file and convert the data to a numpy array
                 if (wf != None):
                     wf.close()
                 
@@ -271,7 +292,8 @@ def update_output(input_filename, submit_times, channel_value, fft_window, frame
                 return *old_figure, 'File does not exist',*cross_mark                 
 
         
-        if input_filename:
+        if input_filename: # Show tick or cross marks to indicate to the user that they are typing in -
+                           # a valid path 
             if input_filename[-1] == '/': # Everytime user enters '/' check if the directory is valid
                 if os.path.exists(input_filename):
                     return *old_figure, '', *tick_mark     
@@ -280,7 +302,7 @@ def update_output(input_filename, submit_times, channel_value, fft_window, frame
         
         return *old_figure, '',None, None
 
-    else:
+    else: # If other than filename field triggered callback call the dsp task and update the charts
         if (wf != None):
             td_fig, fd_fig =  dsp_task( channel_value, 
                                         fft_window, 
@@ -293,6 +315,19 @@ def update_output(input_filename, submit_times, channel_value, fft_window, frame
         
         return *old_figure, '',None, None
             
+
+# Begin: Comment out this section if you don't want to deal with portaudio, sounddevice etc 
+# Callback for playing the selected audio data (orange part selected using range slider)
+import sounddevice as sd
+@app.callback(Output('play', 'n_clicks'), Input('play', 'n_clicks'))
+def play_section(n_clicks):
+    global wf, selected_section
+
+    if wf != None and selected_section.size != 0:
+        sd.play(selected_section, wf.getframerate(), device=sd.default.device[1])
+    
+    return n_clicks # This here hasn't got a purpose - Dash demands that we always return something!
+# End: Comment out this section if you don't want to deal with portaudio, sounddevice etc 
 
 
 update_output.submit_times = 0 # This variable should not be changed outside the update_output function
